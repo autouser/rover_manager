594a1a2bd672c1c92a155614e81e681282e02e71
Evgeny Grep <gyorms@gmail.com>
2016-02-06 15:36:17 +0200

    Add max_x and max_y attributes to Plateau class

--- lib/plateau.rb
+++ lib/plateau.rb
@@ -1,2 +1,26 @@
 class Plateau
+
+  attr_reader :max_x, :max_y
+
+  def initialize(args={})
+    self.max_x = args[:max_x]
+    self.max_y = args[:max_y]
+  end
+
+  def max_x= (arg=nil)
+    raise NotAPositiveInteger unless positive_integer? arg
+    @max_x = arg
+  end
+
+  def max_y= (arg=nil)
+    raise NotAPositiveInteger unless positive_integer? arg
+    @max_y = arg
+  end
+
+  def positive_integer?(arg=nil)
+    !arg.nil? && arg.is_a?(Integer) && (arg > 0)
+  end
+
+  class NotAPositiveInteger < ArgumentError; end
+
 end
--- spec/plateau_spec.rb
+++ spec/plateau_spec.rb
@@ -1,5 +1,81 @@
 require 'spec_helper'
 
 describe Plateau do
-  pending "Not implemented yet"
+
+  let(:plateau) { Plateau.new max_x: 5, max_y: 5 }
+
+  context "#initialize" do
+    context "when attributes are correct" do
+      it "is raises no error" do
+        expect { plateau }.not_to raise_error
+      end
+
+      it "sets attributes" do
+        expect( plateau.max_x ).to eq(5)
+        expect( plateau.max_y ).to eq(5)
+      end
+    end
+  end
+
+
+
+  context "#max_x" do
+
+    context "when is an positive integer" do
+      it "sets attribute" do
+        expect { plateau.max_x = 7 }.not_to raise_error
+        expect( plateau.max_x ).to eq(7)
+      end
+    end
+
+    context "when is not a number" do
+      it "raises 'NotAPositiveInteger' error" do
+        expect { plateau.max_x = 'wrong' }.to raise_error(Plateau::NotAPositiveInteger)
+      end
+    end
+
+    context "when is not an integer" do
+      it "raises 'NotAPositiveInteger' error" do
+        expect { plateau.max_x = 5.5 }.to raise_error(Plateau::NotAPositiveInteger)
+      end
+    end
+
+    context "when is a negative integer" do
+      it "raises 'NotAPositiveInteger' error" do
+        expect { plateau.max_x = -5 }.to raise_error(Plateau::NotAPositiveInteger)
+      end
+    end
+
+  end
+
+
+  context "#max_y" do
+
+    context "when is an positive integer" do
+      it "sets attribute" do
+        expect { plateau.max_y = 7 }.not_to raise_error
+        expect( plateau.max_y ).to eq(7)
+      end
+    end
+
+    context "when is not a number" do
+      it "raises 'NotAPositiveInteger' error" do
+        expect { plateau.max_y = 'wrong' }.to raise_error(Plateau::NotAPositiveInteger)
+      end
+    end
+
+    context "when is not an integer" do
+      it "raises 'NotAPositiveInteger' error" do
+        expect { plateau.max_y = 5.5 }.to raise_error(Plateau::NotAPositiveInteger)
+      end
+    end
+
+    context "when is a negative integer" do
+      it "raises 'NotAPositiveInteger' error" do
+        expect { plateau.max_y = -5 }.to raise_error(Plateau::NotAPositiveInteger)
+      end
+    end
+
+  end
+
 end
